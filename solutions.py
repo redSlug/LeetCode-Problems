ae584a2556233e3b993cdc1b5bf1a23f1e857b31
Bradley Dettmer <11279144+redSlug@users.noreply.github.com>
2018-05-09 15:03:27 -0400

    added another solution

--- solutions.py
+++ solutions.py
@@ -53,11 +53,57 @@ class Solution:
                 nums[i] = 2
                 count_2 -=1
 
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return False
+        tortoise = head.next
+        hare = head.next.next
+        while hare and hare.next:
+            if tortoise == head:
+                return True
+            tortoise = tortoise.next
+            hare = hare.next.next
+        return False
+
+    def hasCycle2(self, head):
+        nodes = set()
+        walker = head
+        while walker:
+            if walker in nodes:
+                return True
+            nodes.add(walker)
+            walker = walker.next
+        return False
+
+
 
 s = Solution()
 assert s.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]) == 6
 assert s.minCostClimbingStairs([10, 15, 20]) == 15
 a = [1, 0, 1, 2]
 s.sortColors(a)
-print(a)
 assert a == [0,1,1,2]
+
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+d = ListNode(4)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = a
+assert s.hasCycle(a) == True
+
+d.next = None
+assert s.hasCycle(a) == False
+
+d.next = a
+assert s.hasCycle2(a) == True
+
+d.next = None
+assert s.hasCycle2(a) == False
